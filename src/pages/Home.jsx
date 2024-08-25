4791cb7fa08429d31437615ecbb535e534121104
Tauya Mtowodzwa <tauyamtowodzwa@gmail.com>
2024-08-25 12:39:18 +0200

    update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,16 +10,17 @@ const Home = () => {
     <Container>
       <div className="bg-primary h-full">
         <div className="w-full h-full bg-gradient-to-r from-primary/10 via-white/30 to-white/10">
-          <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <div className="text-center flex flex-col justify-center pt-5 xl:pt-20 xl:text-left h-full container mx-auto">
             <motion.h1
               variants={fadeIn("down", 0.2)}
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="h1"
+              className="h1 mb-5 leading-none"
             >
-              Transforming Ideas <br /> Into{" "}
-              <span className="text-accent">Digital Reality</span>
+              Crafting Innovative
+              <br /> <span className="text-accent">Solutions</span> From <br />
+              Complex <span className="text-accent">Ideas </span>
             </motion.h1>
 
             <motion.p
@@ -29,9 +30,12 @@ const Home = () => {
               exit="hidden"
               className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
             >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-              delectus sed velit ex repudiandae similique nulla voluptas nisi
-              aliquam distinctio!
+              "I thrive on turning complex ideas into elegant software solutions
+              and I believe in working closely with clients to understand their
+              needs and deliver the best possible outcomes. Through open
+              communication and a collaborative approach, I can translate your
+              vision into user-friendly and effective software that drives real
+              results."
             </motion.p>
 
             <div className="flex justify-center xl:hidden relative">
